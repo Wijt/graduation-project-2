ec65d9c46db868048be4a43a30623df44e03f71b
Furkan Kaya <8fkaya@gmail.com>
2021-04-15 07:57:28 +0300

    Death, Point functions added for Flappy Bird.

--- Assets/FlappyBird/F_Player.cs
+++ Assets/FlappyBird/F_Player.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class F_Player : MonoBehaviour
 {
+    public int point=0;
     public float jumpForce;
     Rigidbody2D rb;
 
@@ -16,5 +18,26 @@ public class F_Player : MonoBehaviour
     {
         rb.velocity = Vector2.zero;
         rb.AddForce(Vector2.up * jumpForce);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponents<F_Pipe>() != null)
+        {
+            point++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Pipe"))
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);       
     }
 }
