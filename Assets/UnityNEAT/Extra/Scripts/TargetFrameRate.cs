1d719ed88bb761474f28515d5b9aa1411dc9c9cd
Furkan Kaya <8fkaya@gmail.com>
2021-05-09 00:59:29 +0300

    Added UnityNEAT library to this project.

---
+++ Assets/UnityNEAT/Extra/Scripts/TargetFrameRate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour{
+
+    void Awake()
+    {
+        Application.targetFrameRate = 300;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Application.targetFrameRate = 300;
+        QualitySettings.vSyncCount = 0;
+	}
+}
